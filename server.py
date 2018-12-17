457bbd09d38a13f82b789f7080d7adf6a9ae1b74
Administrator <admin@example.com>
2018-12-17 15:11:13 +0800

    111

--- server.py
+++ server.py
@@ -1,12 +1,12 @@
-import tornado.ioloop
-import tornado.web
+from tornado.ioloop import IOLoop
+from tornado.web import Application, RequestHandler
 import tornado.httpserver
 
 
 # 主处理模块
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("hi tornado, my name is zhangwenshuo")
+        self.write("好看的皮囊千篇一律,有趣的灵魂万里挑一")
 
 
 if __name__ == "__main__":
@@ -16,7 +16,11 @@ if __name__ == "__main__":
     ])
     # 监听端口,多进程
     http_server = tornado.httpserver.HTTPServer(app)
+    # 绑定端口
     http_server.bind(8000)
-    http_server.start(num_processes=2)
-    tornado.ioloop.IOLoop.instance().start()
+    # 启动进程数
+    http_server.start(2)
+    # 启动Ioloop轮询监听
+    IOLoop.instance().start()
+
 
